5d0e06b42d67079b18b8e4d93f4affc413e0027a
helloshankarb <helloshankar.b@gmail.com>
2025-10-04 18:19:03 +0530

    Updated metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,19 +1,6 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
+const { Platform } = require('react-native');
 
 const config = getDefaultConfig(__dirname);
-
-// Add a specific alias for web platform
-// This will tell Metro to resolve 'react-native-google-mobile-ads' to our mock file
-// when bundling for the 'web' platform.
-config.resolver.extraNodeModules = {
-  ...config.resolver.extraNodeModules,
-  'react-native-google-mobile-ads': path.resolve(__dirname, 'mocks/react-native-google-mobile-ads.js'),
-};
-
-// Keep the blockList as a safeguard, though the extraNodeModules should take precedence for direct imports.
-config.resolver.blockList = [
-  /node_modules\/react-native-google-mobile-ads\/.*/,
-];
 
 module.exports = config;
